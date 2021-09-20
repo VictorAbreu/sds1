3bda64b9a8c055962c2110f002eef8716a20fc71
Victor Abreu <vlabreu@outlook.com>
2021-09-20 18:49:48 -0300

    Adicionando componente Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
-    <h1> Hello </h1>
+    <div>
+
+      <Header/>
+
+    </div>
   );
 }
 
